8a9c20377701e5f89713c13152ee3c5a29aeddb6
kritikaverrma <kritikaverma51510@gmail.com>
2025-07-15 18:53:23 +0530

    fixed search user functionality

--- frontend/src/Components/Chats/SideDrawer.jsx
+++ frontend/src/Components/Chats/SideDrawer.jsx
@@ -95,7 +95,7 @@ function SideDrawer() {
         },
       };
 
-      const { data } = await axios.get(`${process.env.REACT_APP_API_URL}/api/user?search=${search}`, config);
+      const { data } = await axios.get(`${API_URL}/api/user?search=${search}`, config);
 
       setLoading(false);
       setSearchResult(data);
